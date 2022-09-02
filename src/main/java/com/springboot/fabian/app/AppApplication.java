d1c3662a8d4a5b47d458a6e2e8f3ef05cbeab3ff
fabian <fllopezv@ufpso.edu.co>
2022-09-01 21:11:27 -0500

    montaje de archivos del proyecto inicial

---
+++ src/main/java/com/springboot/fabian/app/AppApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.fabian.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Mundo")String name){
+		return "Hola "+name+" en spring boot";
+	}
+}
